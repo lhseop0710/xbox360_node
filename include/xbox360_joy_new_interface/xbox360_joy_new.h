fe086b5a79b42c15150cd24e3abd11b72ffacf93
lhseop0710 <lhseop0710@gmail.com>
2021-10-01 19:28:39 +0900

    first trash

---
+++ include/xbox360_joy_new_interface/xbox360_joy_new.h
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2015 Fadri Furrer, ASL, ETH Zurich, Switzerland
+ * Copyright 2015 Michael Burri, ASL, ETH Zurich, Switzerland
+ * Copyright 2015 Mina Kamel, ASL, ETH Zurich, Switzerland
+ * Copyright 2015 Janosch Nikolic, ASL, ETH Zurich, Switzerland
+ * Copyright 2015 Markus Achtelik, ASL, ETH Zurich, Switzerland
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#ifndef ROTORS_JOY_INTERFACE_JOY_H_
+#define ROTORS_JOY_INTERFACE_JOY_H_
+
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Point.h>
+#include <mav_msgs/RollPitchYawrateThrust.h>
+#include <trajectory_msgs/MultiDOFJointTrajectory.h>
+#include <trajectory_msgs/MultiDOFJointTrajectoryPoint.h>
+#include <ros/ros.h>
+#include <sensor_msgs/Joy.h>
+
+struct Axes {
+  int roll;
+  int pitch;
+  int yaw;
+  int thrust;
+  int roll_direction;
+  int pitch_direction;
+  int yaw_direction;
+  int thrust_direction;
+};
+
+struct Buttons {
+  int takeoff;
+  int land;
+  int ctrl_enable;
+  int ctrl_mode;
+  int yaw_left;
+  int yaw_right;
+};
+
+struct Max {
+  double v_xy;
+  double roll;
+  double pitch;
+  double rate_yaw;
+  double thrust;
+};
+
+class Joy { 
+   typedef sensor_msgs::Joy::_buttons_type ButtonType;
+ private:
+  ros::NodeHandle nh_;
+  ros::Publisher ctrl_pub_;
+  ros::Publisher trajectory_pub_;
+  ros::Subscriber joy_sub_;
+
+  std::string namespace_;
+
+  Axes axes_;
+  Buttons buttons_;
+
+  mav_msgs::RollPitchYawrateThrust control_msg_;
+  trajectory_msgs::MultiDOFJointTrajectory trajectory_msg_;
+  geometry_msgs::PoseStamped pose_;
+
+  sensor_msgs::Joy current_joy_;
+
+  Max max_;
+
+  double current_yaw_vel_;
+  double v_yaw_step_;
+  double desired_position;
+  double desired_yaw;
+
+  void StopMav();
+
+  void JoyCallback(const sensor_msgs::JoyConstPtr& msg);
+  void Publish();
+
+ public:
+  Joy();
+};
+
+#endif // ROTORS_JOY_INTERFACE_JOY_H_
+
---
+++ src/xbox360_joy_new.cpp
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2015 Fadri Furrer, ASL, ETH Zurich, Switzerland
+ * Copyright 2015 Michael Burri, ASL, ETH Zurich, Switzerland
+ * Copyright 2015 Mina Kamel, ASL, ETH Zurich, Switzerland
+ * Copyright 2015 Janosch Nikolic, ASL, ETH Zurich, Switzerland
+ * Copyright 2015 Markus Achtelik, ASL, ETH Zurich, Switzerland
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "xbox360_joy_new_interface/xbox360_joy_new.h"
+#include <mav_msgs/default_topics.h>
+#include <trajectory_msgs/MultiDOFJointTrajectory.h>
+
+
+Joy::Joy() {
+  ros::NodeHandle nh;
+  ros::NodeHandle pnh("~");
+  ctrl_pub_ = nh_.advertise<mav_msgs::RollPitchYawrateThrust> (mav_msgs::default_topics::COMMAND_ROLL_PITCH_YAWRATE_THRUST, 10);
+  trajectory_pub_ = nh.advertise<trajectory_msgs::MultiDOFJointTrajectory>(mav_msgs::default_topics::COMMAND_TRAJECTORY, 10);
+
+//롤피치요쓰러스트 메세지 초기화
+  control_msg_.roll = 0;
+  control_msg_.pitch = 0;
+  control_msg_.yaw_rate = 0;
+  control_msg_.thrust.x = 0;
+  control_msg_.thrust.y = 0;
+  control_msg_.thrust.z = 0;
+  current_yaw_vel_ = 0;
+
+
+//트라젝터리 메세지 포지션 x,y,z,yaw....요는 어케하냐....
+
+  point_.desired_position.x = 0.0;  //doouble형
+  point_.desired_position.y = 0.0;
+  point_.desired_position.z = 0.0;
+  point_.desired_yaw = 0.0;
+
+  // for xbox360 mode2
+  pnh.param("axis_roll_", axes_.roll, 2);
+  pnh.param("axis_pitch_", axes_.pitch, 3);
+  pnh.param("axis_thrust_", axes_.thrust, 1);
+  pnh.param("axis_yaw_", axes_.yaw, 0);
+
+  pnh.param("axis_direction_roll", axes_.roll_direction, -1);
+  pnh.param("axis_direction_pitch", axes_.pitch_direction, 1);
+  pnh.param("axis_direction_yaw", axes_.yaw_direction, 1);
+  pnh.param("axis_direction_thrust", axes_.thrust_direction, 1);
+
+  pnh.param("max_v_xy", max_.v_xy, 3.0);  // [m/s]
+  pnh.param("max_roll", max_.roll, 20.0 * M_PI / 180.0);  // [rad]
+  pnh.param("max_pitch", max_.pitch, 20.0 * M_PI / 180.0);  // [rad]
+  pnh.param("max_yaw_rate", max_.rate_yaw, 300.0 * M_PI / 180.0);  // [rad/s]
+  pnh.param("max_thrust", max_.thrust, 30.0);  // [N]
+
+  pnh.param("v_yaw_step", v_yaw_step_, 0.1);  // [rad/s]
+  
+  // for xbox360
+  pnh.param("button_yaw_left_", buttons_.yaw_left, 4);
+  pnh.param("button_yaw_right_", buttons_.yaw_right, 5);
+  pnh.param("button_ctrl_enable_", buttons_.ctrl_enable, 2);
+  pnh.param("button_ctrl_mode_", buttons_.ctrl_mode, 1);
+  pnh.param("button_takeoff_", buttons_.takeoff, 3);
+  pnh.param("button_land_", buttons_.land, 0);
+
+  // Overwrite defaults if set as node parameters.
+  pnh.param("x", desired_position.x, 0);
+  pnh.param("y", desired_position.y, 0);
+  pnh.param("z", desired_position.z, 0);
+  pnh.param("yaw", desired_yaw, 0);
+
+  namespace_ = nh_.getNamespace();
+  joy_sub_ = nh_.subscribe("joy", 10, &Joy::JoyCallback, this);
+}
+
+void Joy::StopMav() {
+
+  control_msg_.roll = 0;
+  control_msg_.pitch = 0;
+  control_msg_.yaw_rate = 0;
+  control_msg_.thrust.x = 0;
+  control_msg_.thrust.y = 0;
+  control_msg_.thrust.z = 0;
+}
+
+void Joy::JoyCallback(const sensor_msgs::JoyConstPtr& msg) {
+  current_joy_ = *msg;
+
+  control_msg_.roll = msg->axes[axes_.roll] * max_.roll * axes_.roll_direction;
+  control_msg_.pitch = msg->axes[axes_.pitch] * max_.pitch * axes_.pitch_direction;
+  control_msg_.thrust.z = (msg->axes[axes_.thrust] + 1) * max_.thrust / 2.0 * axes_.thrust_direction;
+  control_msg_.yaw_rate = msg->axes[axes_.yaw] * max_.rate_yaw * axes_.yaw_direction;
+
+  point_.desired_position.x = 2.5*control_msg_.pitch;
+  point_.desired_position.y = 2.5*control_msg_.roll;	// ROS y axis pos to the left
+  point_.desired_position.z = 0.1*(msg->axes[axes_.thrust] * max_.thrust / 2.0 * axes_.thrust_direction);	
+  point_.desired_yaw = -control_msg_.yaw_rate;	
+
+
+
+  ros::Time update_time = ros::Time::now();
+  control_msg_.header.stamp = update_time;
+  control_msg_.header.frame_id = "rotors_joy_frame";
+
+  trajectory_msg_.header.stamp = update_time;
+  trajectory_msg_.header.frame_id = "joy_position_frame";
+
+  Publish();
+
+}
+
+void Joy::Publish() {
+  ctrl_pub_.publish(control_msg_);
+  trajectory_pub_.publish(trajectory_msg_);
+}
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "xbox360_joy_interface");
+  Joy joy;
+
+  ros::spin();
+
+  return 0;
+}
